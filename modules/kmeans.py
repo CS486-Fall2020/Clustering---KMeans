2d6aec0b781710f89b087153570dee29da175ef5
nedimazar <nedimazar@hotmail.com>
2020-09-19 17:06:04 +0300

    KMeans works, need a better way of picking random points to initialize centroids.

--- modules/kmeans.py
+++ modules/kmeans.py
@@ -12,46 +12,55 @@ class kmeans(cluster):
         self.__max_iterations = max_iterations
 
     #This is the euclidean distance between x and y
-    def dist(self, x, y):
-        return (math.sqrt(sum([(a - b) ** 2 for a, b in zip(x, y)])))
+    def __dist(self, x, y):
+        return (math.sqrt(abs(sum([(a - b) ** 2 for a, b in zip(x, y)]))))
 
-        
+
+
+    def __get_cluster_hypotheses(self, X, centroids):
+        cluster_hypotheses = []
+
+        for x in X:
+            distances = []
+
+            #Get distance to each centroid
+            for c in centroids:
+                distances.append(self.__dist(x, c))
+
+            #Keep the minimum distance
+            cluster_hypotheses.append(distances.index(min(distances)))
+        return cluster_hypotheses
+
+    
+    def __get_potential_clusters(self, X, cluster_hypotheses):
+        potential_clusters = {}
+
+        #hyp is the cluster hypothesis
+        #x is the data point corresponding to that hypothesis
+        for hyp, x in zip(cluster_hypotheses, X):
+                if hyp not in potential_clusters:
+                    potential_clusters[hyp] = [x]
+                else:
+                    potential_clusters[hyp].append(x)
+
+        return potential_clusters
+
+    
     def fit(self, X):
-
         #Picking centroids randomly in the data
         centroids = list(np.array(random.choices(X, k=self.__k)))
 
         #We will run the algorithm the maximum amount of times specified
         for w in range(self.__max_iterations):
-            #Initiate cluster hypotheses to -1 since we don't know yet
-            cluster_hypotheses = []
 
-            #For each point let's get the distances to each centroid
-            for x in X:
-                curr_dist = sys.float_info.max
-                prev_dist = sys.float_info.max
-                d = sys.float_info.max
-                
-                for j, c in enumerate(centroids):
-                
-                    curr_dist = self.dist(x, c)
-                    d = j if curr_dist < prev_dist else d
-                    prev_dist = curr_dist
-                cluster_hypotheses.append(d)
+            #Get the cluster hypotheses for current centroids
+            cluster_hypotheses = self.__get_cluster_hypotheses(X, centroids)
             
-            potential_clusters = {}
-            
-            for hyp, x in zip(cluster_hypotheses, X):
-                if hyp not in potential_clusters:
-                    potential_clusters[hyp] = [x]
-                else:
-                    potential_clusters[hyp].append(x)
-            
+            #Separating ech point into it's cluster to calculate new centroids
+            potential_clusters = self.__get_potential_clusters(X, cluster_hypotheses)
+
+            #Taking the mean of each cluster and assigning it to its centroid
             for key in potential_clusters:
                 centroids[key] = np.mean(potential_clusters[key], axis=0)
-
-        
-        #return(cluster_hypotheses)
-        #print(cluster_hypotheses)
 
         return([cluster_hypotheses] + [centroids])
